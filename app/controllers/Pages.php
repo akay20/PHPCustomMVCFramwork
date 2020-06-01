bab7f6cbca0a89649a19a9196d089cf724614473
akshay <akshay@ssseurope.onmicrosoft.com>
2020-06-01 18:39:36 +0530

    post add

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -7,6 +7,10 @@ class Pages extends Controller {
     }
 
     public function index(){
+
+        if(isLoggedIn()){
+            redirect('posts');
+        }
     	
     	$data = [
             'title' => 'SharePosts',
--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -26,6 +26,57 @@ class Posts extends Controller {
         $this->view('posts/index', $data);
     }
 
+    public function add(){
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST' ) {
+            
+            //Sanitize post array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_err' => '',
+                'body_err' => ''
+            ];
+
+            //Validate Data
+            if(empty($data['title'])){
+                $data['title_err'] = 'Please Enter title';
+            }
+            if(empty($data['body'])){
+                $data['body_err'] = 'Please enter body text';
+            }
+
+            if(empty($data['title_err']) && empty($data['body_err'])){
+                // Validate
+                if($this->postModel->addPosts($data)){
+                    flash('post_message', 'Post Added');
+                    redirect('posts');
+                } else {
+                    die('Something Went wrong!');
+                }
+
+            } else {
+                // Load view with errors
+                $this->view('posts/add', $data);
+            }
+
+        } else {
+            $data = [
+                'title' => '',
+                'body' => ''
+            ];
+
+            $this->view('posts/add', $data);
+        }
+    }
+
+    public function show($id){
+        
+    }
+
 }
 
 ?>
